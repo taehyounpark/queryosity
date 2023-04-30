29a21dc5e03291a9b436cf5d3ddd8389f4e2cd7e
Tae Hyoun Park <tae.hyoun.park@cern.ch>
2023-04-29 23:52:42 -0700

    CMake

--- cmake/standalone.cmake
+++ cmake/standalone.cmake
@@ -1,8 +1,12 @@
 # register the shared object to include both sources and dictionaries
-include_directories ("${PROJECT_SOURCE_DIR}")
+project(ana)
+include_directories("${PROJECT_SOURCE_DIR}")
+
+file(GLOB ana_headers ana/*.h*)
+file(GLOB ana_sources src/*.cxx)
 
 # register the shared object
-add_library( ana SHARED ${anaSources} ${anaHeaders})
+add_library( ana SHARED ${ana_sources} ${ana_headers})
 
 # link everything together at the end
 target_include_directories(
@@ -15,7 +19,7 @@ set_target_properties(
   ana
   PROPERTIES
   VERSION 0.1
-  PUBLIC_HEADER "${anaHeaders}"
+  PUBLIC_HEADER "${ana_headers}"
 )
 target_compile_features(ana PRIVATE cxx_std_17)
 target_link_libraries( ana Threads::Threads )
@@ -46,6 +50,20 @@ install(DIRECTORY
 
 message(${CMAKE_CURRENT_BINARY_DIR})
 
+# link everything together at the end
+install(
+  TARGETS ana
+  EXPORT config
+  DESTINATION lib
+)
+
 set(ANA_PYTHONPATH ${CMAKE_CURRENT_BINARY_DIR})
 set(ANA_LD_LIBRARY_PATH ${CMAKE_CURRENT_BINARY_DIR})
+set(SETUP ${CMAKE_CURRENT_BINARY_DIR}/setup.sh)
+
+# write setup script
+file(WRITE ${SETUP} "#!/bin/bash\n")
+file(APPEND ${SETUP} "ulimit -S -s unlimited\n" )
+file(APPEND ${SETUP} "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:${ANA_LD_LIBRARY_PATH}\n")
+file(APPEND ${SETUP} "export DYLD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:${ANA_LD_LIBRARY_PATH}\n")
 
