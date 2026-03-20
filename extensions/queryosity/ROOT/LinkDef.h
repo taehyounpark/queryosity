2486a77374dd76d44f4eb9b51047dbb2ec460a13
Tae Hyoun Park <tae.hyoun.park@cern.ch>
2026-03-20 22:39:00 +0100

    LInkDef

--- extensions/queryosity/ROOT/LinkDef.h
+++ extensions/queryosity/ROOT/LinkDef.h
@@ -12,9 +12,9 @@
 
 #ifdef __CLING__
 
-// #pragma link off all globals;
-// #pragma link off all classes;
-// #pragma link off all functions;
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
 
 #pragma link C++ nestedclasses;
 #pragma link C++ nestedtypedefs;
